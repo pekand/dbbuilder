9d877f893ded580f9afe1630f62c89cc45ab378e
Andrej Pekar <pekand@gmail.com>
2019-03-24 17:43:02 +0100

    new favicon

--- Modules/Main/Templates/mainlayoutTemplate.php
+++ Modules/Main/Templates/mainlayoutTemplate.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     <title><?php $this->blockInsert("title", "DbBuilder")?></title>
-    <link rel='shortcut icon' type='image/x-icon' href='/assets/img/favicon.ico' />
+    <link rel='shortcut icon' type='image/x-icon' href='/assets/favicon.ico' />
 
     <link rel="stylesheet" href="/assets/css/lib/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
